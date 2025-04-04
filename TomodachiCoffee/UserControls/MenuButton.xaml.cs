3acf6d42d667ba747fc16b4f961640c02c483aad
jsand <jsand@JAVIER>
2025-04-04 16:23:57 -0300

    Se termina diseño de dashboard

---
+++ TomodachiCoffee/UserControls/MenuButton.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.IconPacks;
+
+namespace TomodachiCoffee.UserControls
+{
+    /// <summary>
+    /// Lógica de interacción para MenuButton.xaml
+    /// </summary>
+    public partial class MenuButton : UserControl
+    {
+        public MenuButton()
+        {
+            InitializeComponent();
+        }
+
+        public PackIconMaterialKind Icon
+        {
+            get { return (PackIconMaterialKind) GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }  
+        }
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(PackIconMaterialKind), typeof(MenuButton));
+
+        public bool IsActive
+        {
+            get { return (bool)GetValue(IsActiveProperty); }
+            set { SetValue(IsActiveProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsActiveProperty =
+            DependencyProperty.Register("IsActive", typeof(bool), typeof(MenuButton));
+    }
+}
