704838e61e0ba257076f1758ae0450ef7adde00f
jsand <jsand@JAVIER>
2025-04-12 13:46:29 -0400

    Se crea navegación a nivel dashboard

--- TomodachiCoffee/Models/ViewModel.cs
+++ TomodachiCoffee/Models/ViewModel.cs
@@ -3,27 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Input;
+using TomodachiCoffee.UserControls;
 
 namespace TomodachiCoffee.Models
 {
-    public class ViewModel
+    public class ViewModel : ViewModelBase
     {
         public ICommand NavigateCommand { get; }
         public ICommand LogoutCommand { get; }
 
         public Action LogoutAction { get; set; }
 
+        private UserControl _currentView;
+
+        public UserControl CurrentView
+        {
+            get => _currentView;
+            set => SetProperty(ref _currentView, value);
+        }
+
         public ViewModel()
         {
             NavigateCommand = new RelayCommand<string>(NavigateTo);
             LogoutCommand = new RelayCommand(Logout);
+
+            // Establecer vista por defecto al iniciar
+            CurrentView = new HomeView();
         }
 
         private void NavigateTo(string page)
         {
             Console.WriteLine($"Navegando a {page}");
             // Aquí puedes cambiar la vista o hacer alguna acción específica
+
+            switch (page)
+            {
+                case "Home":
+                    CurrentView = new HomeView();
+                    break;
+                case "Finance":
+                    // Agrega más vistas según las tengas
+                    // CurrentView = new FinanceView();
+                    break;
+                default:
+                    break;
+            }
         }
 
         private void Logout()
---
+++ TomodachiCoffee/Models/ViewModelBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomodachiCoffee.Models
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T backingField, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(backingField, value))
+                return false;
+
+            backingField = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
