f4ccc5307470d8f6aa90fa043d1c2bdb7ceae40e
jsand <jsand@JAVIER>
2025-04-13 18:31:38 -0400

    Se adapta grafico a tabla reporte para ver su comportamiento

--- TomodachiCoffee/ViewModels/HomeViewModel.cs
+++ TomodachiCoffee/ViewModels/HomeViewModel.cs
@@ -1,4 +1,8 @@
 ﻿using System.Collections.ObjectModel;
+using System.Data;
+using System.Windows.Media;
+using LiveCharts;
+using LiveCharts.Wpf;
 using TomodachiCoffee.Models;
 
 namespace TomodachiCoffee.ViewModels
@@ -13,22 +17,38 @@ namespace TomodachiCoffee.ViewModels
             set => SetProperty(ref _productos, value);
         }
 
+        public SeriesCollection MesSeries { get; set; }
+        public string[] MesLabels { get; set; }
+
         public HomeViewModel()
         {
-            CargarProductos();
-        }
+            var datos = new List<(string Mes, double Total)>();
 
-        private void CargarProductos()
-        {
-            var productos = new ObservableCollection<string>();
-            var dt = Database.ExecuteQuery("SELECT Total FROM reporte");
+            var dt = Database.ExecuteQuery(@"
+                SELECT month(STR_TO_DATE(Fecha, '%d-%m-%Y')), left(monthname(STR_TO_DATE(Fecha, '%d-%m-%Y')), 3) as Mes, SUM(Total) AS Total 
+                FROM bd_cafeteria.reporte 
+                WHERE STR_TO_DATE(Fecha, '%d-%m-%Y') >= DATE_SUB(CURDATE(), INTERVAL 12 MONTH)
+                GROUP BY month(STR_TO_DATE(Fecha, '%d-%m-%Y')), left(monthname(STR_TO_DATE(Fecha, '%d-%m-%Y')), 3)
+                ORDER BY month(STR_TO_DATE(Fecha, '%d-%m-%Y')) ASC
+            ");
 
-            foreach (System.Data.DataRow row in dt.Rows)
+            foreach (DataRow row in dt.Rows)
             {
-                productos.Add(row["Total"].ToString());
+                string medioPago = row["Mes"].ToString();
+                double total = Convert.ToDouble(row["Total"]);
+                datos.Add((medioPago, total));
             }
 
-            Productos = productos;
+            MesLabels = datos.Select(d => d.Mes).ToArray();
+            MesSeries = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title = "Ventas x Mes",
+                    Values = new ChartValues<double>(datos.Select(d => (double)d.Total)),
+                    Fill = (Brush)new BrushConverter().ConvertFromString("#7166f9")
+                }
+            };
         }
     }
 }
